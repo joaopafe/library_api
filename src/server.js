515b6966a27dac879403d3d981197536afea4d32
João Paulo <joao.ea2020@gmail.com>
2024-02-11 14:36:28 -0300

    add inicializacao da api

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const env = require("../setup");
+const express = require("express");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(port, console.log("Servidor na porta " + port));
